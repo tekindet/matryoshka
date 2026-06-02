f3ab627c52d15d8da9a9eda1b4a6824286f23128
aitumik <aitumik@proton.me>
2026-06-02 16:13:27 +0300

    feat(orch): implement deploy service feature

--- internal/orchestrator/docker.go
+++ internal/orchestrator/docker.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/tekindet/matryoshka/internal/domain"
 )
 
@@ -32,5 +35,68 @@ func (d *DockerOrchestrator) CreateNetwork(ctx context.Context, name string) (st
 }
 
 func (d *DockerOrchestrator) DeployService(ctx context.Context, svc *domain.Service, networkName string) (string, error) {
-	return "not-yet-implemented", nil
+	var image string
+	var env []string
+	var exposedPorts nat.PortSet
+
+	switch svc.Type {
+	case domain.ServiceTypePostgres:
+		image = "postgres:15-alpine"
+		env = []string{
+			"POSTGRES_PASSWORD=postgres",
+			"POSTGRES_USER=postgres",
+			"POSTGRES_DB=postgres",
+		}
+		exposedPorts = nat.PortSet{"5435/tcp": struct{}{}}
+
+	case domain.ServiceTypeRedis:
+		image = "redis:7-alpine"
+		exposedPorts = nat.PortSet{"6379/tcp": struct{}{}}
+
+	case domain.ServiceTypeApp:
+		return "", fmt.Errorf("service not implemented yet")
+
+	case domain.ServiceTypeQueue:
+		return "", fmt.Errorf("service not implemented yet")
+
+	case domain.ServiceTypeWorker:
+		return "", fmt.Errorf("service not implemented yet")
+
+	default:
+		return "", fmt.Errorf("service not implemented yet")
+	}
+
+	config := &container.Config{
+		Image:        image,
+		Env:          env,
+		ExposedPorts: exposedPorts,
+		Hostname:     svc.Name,
+	}
+
+	hostConfig := &container.HostConfig{
+		AutoRemove:  true,
+		NetworkMode: container.NetworkMode(networkName),
+	}
+
+	containerName := fmt.Sprintf("%s-%s", svc.Name, svc.ID[:6])
+
+	res, err := d.cli.ContainerCreate(
+		ctx,
+		config,
+		hostConfig,
+		&network.NetworkingConfig{},
+		&v1.Platform{Architecture: "x86_64"},
+		containerName,
+	)
+
+	if err != nil {
+		return "", fmt.Errorf("failed creating contianer configuration")
+	}
+
+	err = d.cli.ContainerStart(context.Background(), res.ID, container.StartOptions{})
+	if err != nil {
+		return "", fmt.Errorf("failed starting container infrastructure : %w", err)
+	}
+
+	return res.ID, nil
 }
--- internal/orchestrator/docker_test.go
+++ internal/orchestrator/docker_test.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"testing"
 
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
+	"github.com/tekindet/matryoshka/internal/domain"
 	"github.com/tekindet/matryoshka/internal/orchestrator"
 )
 
@@ -43,4 +45,56 @@ func TestOrchestrator_CreateNetwork(t *testing.T) {
 	if netResource.Name != networkName {
 		t.Errorf("wanted network name to be %s got %s", networkName, netResource.Name)
 	}
+}
+
+func TestOrchestrator_DeployService_Redis(t *testing.T) {
+
+	cli, err := client.NewClientWithOpts(
+		client.WithHost("unix:///var/run/docker.sock"),
+		client.WithAPIVersionNegotiation(),
+	)
+	if err != nil {
+		t.Fatalf("could not create a docker client %v", err)
+	}
+	defer cli.Close()
+
+	orch := orchestrator.NewDockerOrchestrator(cli)
+	ctx := context.Background()
+
+	networkName := "test-project-isolated-net"
+
+	_, err = orch.CreateNetwork(ctx, networkName)
+	if err != nil {
+		t.Skip("failed to create network, might already exist")
+	}
+
+	defer cli.NetworkRemove(ctx, networkName)
+
+	mockSvc := &domain.Service{
+		ID:        "abc-123-xyz-789-longstring",
+		ProjectID: "proj-123",
+		Name:      "test-redis-cache",
+		Type:      domain.ServiceTypeRedis,
+	}
+
+	containerID, err := orch.DeployService(ctx, mockSvc, networkName)
+	if err != nil {
+		t.Fatalf("expected service to deploy onto docker cleanly, got: %v", err)
+	}
+
+	if containerID == "" {
+		t.Error("expected a functional system container ID handle")
+	}
+
+	defer cli.ContainerRemove(ctx, containerID, container.RemoveOptions{Force: true})
+
+	inspect, err := cli.ContainerInspect(ctx, containerID)
+	if err != nil {
+		t.Fatalf("failed to inspect launched container infrastructure: %v", err)
+	}
+
+	if !inspect.State.Running {
+		t.Errorf("expected container to be state:RUNNING, instead found status: %s", inspect.State.Status)
+	}
+
 }
